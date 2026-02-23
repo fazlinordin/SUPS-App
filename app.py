64840562b54607dbb6b0e0244488b9e21cacaba2
fazlinordin <m.fazli@moh.gov.my>
2026-02-24 01:26:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,52 +1,50 @@
 import streamlit as st
 import pandas as pd
 import requests
-import re
 from datetime import datetime, date
 
-st.set_page_config(page_title="SUPS HJEM V3.5", layout="wide")
+st.set_page_config(page_title="SUPS HJEM V3.6", layout="wide")
 
 URL_API = "https://script.google.com/macros/s/AKfycbzir4NpkjGqR7XuBTFfxg8tziu7fBSlrHKgUICM_KSfC0MnRScdXh_8oi7uTGfHe01mkg/exec"
 URL_SHEET_CSV = "https://docs.google.com/spreadsheets/d/18K_lW1HUvA28cG6b5tf9RR3ckF8ONyALzDejvMhTvtI/export?format=csv"
 
-# --- MASTER LIST (131 UBAT) ---
+# --- SENARAI RASMI 131 UBAT (Pastikan Nama Unik & Konsisten) ---
 MASTER_UBAT = sorted([
-    "acetazolamide 250mg tab", "acetylsalicyclic acid 150 mg dispersible tab", "Acitretin 25mg capsule", "ACTRAPID",
-    "acyclovir 800mg tab", "adapalene 0.1% gel", "allopurinol 100mg tablet", "amlodipine 10mg + valsartan 160",
-    "amorolfine 5% nail lacquer", "Apixaban 2.5mg film coated tablet", "apixaban 5mg film coated tablet", "aqueous cream",
-    "aripiprazole 10mg", "artificial tears/eye lubricant opth sol (single use)", "artificial tears/eye lubricant opthalmic solution",
-    "ascorbic acid 100mg", "atorvastatin 20 mg", "atorvastatin 40mg", "baclofen 10 mg", 
-    "beclomethasone dipr 100mcg, formoterol 6mcg inh.", "benzoyl peroxide 5% gel", "betametasone 17 valerate 1 in 4 cream (0.025%)",
-    "betametasone 17 valerate 1 in 2 cream (0.05%)", "betametasone valearate 1:2 oinment", "betamethasone 17 valerate 0.1% ointment",
-    "betamethasone 17-valerate 0.1% cream", "brimonidine tartrate 0.15% opth sol.", 
-    "budesonide 160mcg and formeterol 4.5mcg turbuhaler 120 doses", "calcipotrio 50 mcg/g betametasone 0.5mg/g oint",
-    "calcitriol 0.25 mcg", "calcium carbonate 500mg", "calcium lactate 300mg", "carbamazepine 400mg CR", "Carbamide (urea) 10% Cream",
-    "celexocib 200mg", "cetrimide 2% lotion", "cetrizine hcl 10mg", "Clobetasol Propionate 0.05% Oint.", "clobetasone butyrate 0.05% cream",
-    "clobetasone butyrate 0.05% oinmt", "clopidogrel 75mg tab", "coal tar (LPC) 3% ointment", "coal tar (lpc) 6% oinment",
-    "coal tar 1 % salicyclic acid 2 % shampoo (sebitar)", "Coal Tar 12% salicylic acid 2% Sulphur 4% Oint.", "dabigatran etexilate 150mg cap",
-    "dapagliflozin 10mg", "dexamethasone sodium phosphate 0.1% eye drops", "dexamethasone,neomycin,polymyxin B eyedrop (maxitrol)",
-    "diltiazem 30mg", "dorzolamide hcl 2% opth. Sol.", "dutasteride 0.5mg + tamsulosin 0.4mg cap", "dydrogesterone 10 mg tab",
-    "empagliflozin 25mg tab", "emulsifying ointment BP (emulsificant oint)", "ezetimibe 10 mg tab", "felodipine 10mg ER", "fenofibrate 145mg",
-    "ferric ammonium citrate 400mg/5ml (FAC)", "finasteride 5 mg", "flupenthixol decoante depot 20mg/ml inj",
-    "fluticasone propionate 125mcg/dose evohaler", "fluvoxamine 50mg", "fusidic acid 1% eye drops", "gabapentin 300mg",
-    "gemfibrozil 300mg", "gliclazide 80 mg", "gliclazide mr 60mg", "hypromellose 0.3% eye drop (preser. Fre)", "IBERET FOLIC",
-    "insugen 30/70", "insugen N", "INSUGEN R", "INSULATARD", "Insulin glargine lantus", "INSUPEN",
-    "Ipratropium br 20mcg fenoterol 50mcg/dose BERODUAL", "ivabradine 5 mg", "ketoprofen 2.5% gel", "lactulose 3.35g/5 ml liquid",
-    "lamotrigine 100mg tab", "Lamotrigine 50mg tab", "latanaprost 0.005% eye drop", "levetiracetam 500mg", "loratadine 10mg",
-    "lorazepam 1mg", "metformin xr 750 mg", "methotrexate 2.5mg", "mirabegron 50 mg PROLONGED RELEASE tab", "mirtazapine 15 mg orodispersible tab",
-    "mirtazapine 30 mg orodispersible tab", "mirtazapine 15mg orodispersible tab", "mirtazapine 30mg orodispersible tab", "MIXTARD", 
-    "mometasone furoate 0.1% cream", "montelukast 10 mg", "moxifloxacin 0.5% ophthalmic solution",
-    "nepafenac 0.1% w/v eye suspension (5ml)", "olanzapine 10mg tab", "omeprazole 20mg capsule", "paliperidone 75mg prolonged release injection",
-    "pantoprazole 40mg", "pine tar 1% coal tar 1% salicyclic acid 2% shampoo sebitar", "quetiapine fumarate 100mg IR tab",
-    "quetiapine fumarate 200mg ER", "quetiapine fumarate 50mg extended release tab", "salicyclic acid 5% oinment",
-    "salicyclic acid, sulphur and liquid coal tar ointment", "salicylic acid 10% ointment", "salmeterol 25mcg , fluticasone propionate 125mcg evohaler",
-    "salmeterol 50mcg +fluticasone 500mcg accuhaler", "Simvastatin 40 mg", "sodium bicarbonate, citric acid, sod citrate, tartaric acid 4g/sachet",
-    "sodium valproate 200mg/5ml syrup (EPILIM)", "spironolactone 25mg", "sulphamethoxazole 400mg trimethoprim 80mg tab", 
-    "sunscreen SPF 50 lotion/cream", "tamsulosin HCL 400mcg ER", "theophyline sr 250mg", "timolol maleate 0.5% eye drop (pres. Free) 10 ml", 
-    "tiotropium 2.5mcg & olodaterol 2.5mcg/actuation,inh(Ctrdge only)", "tiotropium 2.5mcg / puff inhalation (catrigde only)", 
-    "Topiramate 100mg", "tramadol 50mg", "tretinoin 0.05% cream", "ursodeoxycholic acid 250mg capsule", "valsartan 160mg", 
-    "valsartan 80mg", "verapamil 40mg tab", "vildagliptin 50 mg tab", "vitamin b1 b6 b12", "warfarin 1mg", "Warfarin 2mg", 
-    "Warfarin 5mg", "white petroleum anhydrous liq linolin, mineral oil eye oint", "White Soft Paraffin BP (White Petroleum Jelly BP)"
+    "ACETAZOLAMIDE 250MG TAB", "ACETYLSALICYCLIC ACID 150 MG DISPERSIBLE TAB", "ACITRETIN 25MG CAPSULE", "ACTRAPID",
+    "ACYCLOVIR 800MG TAB", "ADAPALENE 0.1% GEL", "ALLOPURINOL 100MG TABLET", "AMLODIPINE 10MG + VALSARTAN 160",
+    "AMOROLFINE 5% NAIL LACQUER", "APIXABAN 2.5MG FILM COATED TABLET", "APIXABAN 5MG FILM COATED TABLET", "AQUEOUS CREAM",
+    "ARIPIPRAZOLE 10MG", "ARTIFICIAL TEARS EYE LUBRICANT (SINGLE USE)", "ARTIFICIAL TEARS EYE LUBRICANT SOLUTION",
+    "ASCORBIC ACID 100MG", "ATORVASTATIN 20 MG", "ATORVASTATIN 40MG", "BACLOFEN 10 MG", 
+    "BECLOMETHASONE DIPR 100MCG, FORMOTEROL 6MCG INH.", "BENZOYL PEROXIDE 5% GEL", "BETAMETASONE 17 VALERATE 1 IN 4 CREAM (0.025%)",
+    "BETAMETASONE 17 VALERATE 1 IN 2 CREAM (0.05%)", "BETAMETASONE VALEARATE 1:2 OINMENT", "BETAMETHASONE 17 VALERATE 0.1% OINTMENT",
+    "BETAMETHASONE 17-VALERATE 0.1% CREAM", "BRIMONIDINE TARTRATE 0.15% OPTH SOL.", 
+    "BUDESONIDE 160MCG AND FORMETEROL 4.5MCG TURBUHALER 120 DOSES", "CALCIPOTRIO 50 MCG/G BETAMETASONE 0.5MG/G OINT",
+    "CALCITRIOL 0.25 MCG", "CALCIUM CARBONATE 500MG", "CALCIUM LACTATE 300MG", "CARBAMAZEPINE 400MG CR", "CARBAMIDE (UREA) 10% CREAM",
+    "CELEXOCIB 200MG", "CETRIMIDE 2% LOTION", "CETRIZINE HCL 10MG", "CLOBETASOL PROPIONATE 0.05% OINT.", "CLOBETASONE BUTYRATE 0.05% CREAM",
+    "CLOBETASONE BUTYRATE 0.05% OINMT", "CLOPIDOGREL 75MG TAB", "COAL TAR (LPC) 3% OINTMENT", "COAL TAR (LPC) 6% OINMENT",
+    "COAL TAR 1 % SALICYCLIC ACID 2 % SHAMPOO", "COAL TAR 12% SALICYLIC ACID 2% SULPHUR 4% OINT.", "DABIGATRAN ETEXILATE 150MG CAP",
+    "DAPAGLIFLOZIN 10MG", "DEXAMETHASONE SODIUM PHOSPHATE 0.1% EYE DROPS", "DEXAMETHASONE,NEOMYCIN,POLYMYXIN B (MAXITROL)",
+    "DILTIAZEM 30MG", "DORZOLAMIDE HCL 2% OPTH. SOL.", "DUTASTERIDE 0.5MG + TAMSULOSIN 0.4MG CAP", "DYDROGESTERONE 10 MG TAB",
+    "EMPAGLIFLOZIN 25MG TAB", "EMULSIFYING OINTMENT BP", "EZETIMIBE 10 MG TAB", "FELODIPINE 10MG ER", "FENOFIBRATE 145MG",
+    "FERRIC AMMONIUM CITRATE 400MG/5ML", "FINASTERIDE 5 MG", "FLUPENTHIXOL DECOANTE DEPOT 20MG/ML INJ",
+    "FLUTICASONE PROPIONATE 125MCG/DOSE EVOHALER", "FLUVOXAMINE 50MG", "FUSIDIC ACID 1% EYE DROPS", "GABAPENTIN 300MG",
+    "GEMFIBROZIL 300MG", "GLICLAZIDE 80 MG", "GLICLAZIDE MR 60MG", "HYPROMELLOSE 0.3% EYE DROP", "IBERET FOLIC",
+    "INSUGEN 30/70", "INSUGEN N", "INSUGEN R", "INSULATARD", "INSULIN GLARGINE LANTUS", "INSUPEN",
+    "BERODUAL INHALER", "IVABRADINE 5 MG", "KETOPROFEN 2.5% GEL", "LACTULOSE LIQUID",
+    "LAMOTRIGINE 100MG TAB", "LAMOTRIGINE 50MG TAB", "LATANAPROST 0.005% EYE DROP", "LEVETIRACETAM 500MG", "LORATADINE 10MG",
+    "LORAZEPAM 1MG", "METFORMIN XR 750 MG", "METHOTREXATE 2.5MG", "MIRABEGRON 50 MG", "MIRTAZAPINE 15 MG",
+    "MIRTAZAPINE 30 MG", "MIXTARD", "MOMETASONE FUROATE 0.1% CREAM", "MONTELUKAST 10 MG", "MOXIFLOXACIN 0.5% OPHTHALMIC SOLUTION",
+    "NEPAFENAC 0.1% W/V EYE SUSPENSION", "OLANZAPINE 10MG TAB", "OMEPRAZOLE 20MG CAPSULE", "PALIPERIDONE INJECTION",
+    "PANTOPRAZOLE 40MG", "SEBITAR SHAMPOO", "QUETIAPINE FUMARATE 100MG IR",
+    "QUETIAPINE FUMARATE 200MG ER", "QUETIAPINE FUMARATE 50MG ER", "SALICYCLIC ACID 5% OINMENT",
+    "SALICYCLIC ACID, SULPHUR AND LIQUID COAL TAR", "SALICYLIC ACID 10% OINTMENT", "SALMETEROL 25/FLUTICASONE 125 EVOHALER",
+    "SALMETEROL 50/FLUTICASONE 500 ACCUHALER", "SIMVASTATIN 40 MG", "URINARY ALKALINIZER SACHET",
+    "SODIUM VALPROATE SYRUP", "SPIRONOLACTONE 25MG", "COTRIMOXAZOLE 480MG TAB", 
+    "SUNSCREEN SPF 50", "TAMSULOSIN HCL 400MCG ER", "THEOPHYLINE SR 250MG", "TIMOLOL MALEATE 0.5% EYE DROP", 
+    "TIOTROPIUM & OLODATEROL RESPIMAT", "TIOTROPIUM RESPIMAT", 
+    "TOPIRAMATE 100MG", "TRAMADOL 50MG", "TRETINOIN 0.05% CREAM", "URSODEOXYCHOLIC ACID 250MG CAPSULE", "VALSARTAN 160MG", 
+    "VALSARTAN 80MG", "VERAPAMIL 40MG TAB", "VILDAGLIPTIN 50 MG TAB", "VITAMIN B1 B6 B12", "WARFARIN 1MG", "WARFARIN 2MG", 
+    "WARFARIN 5MG", "WHITE PETROLEUM EYE OINT", "WHITE PETROLEUM JELLY"
 ])
 
 def load_data():
@@ -56,78 +54,81 @@ def load_data():
         return df
     except: return pd.DataFrame()
 
-def ekstrak_angka(teks):
-    # Mengambil hanya nombor bulat dari teks kuantiti
-    angka = re.findall(r'\d+', str(teks))
-    return sum(int(a) for a in angka) if angka else 0
+def convert_to_matrix_full(df_filtered):
+    # 1. Bina senarai ubat asas (Semua 131 ubat mesti ada)
+    matrix = pd.DataFrame(index=MASTER_UBAT)
+    matrix.index.name = "💊 NAMA UBAT"
+    
+    if df_filtered.empty:
+        return matrix
 
-def convert_to_matrix_with_total(df_filtered):
-    data_list = []
+    # 2. Proses data pesakit
+    pesakit_data = []
     for _, row in df_filtered.iterrows():
-        ubats = str(row['UBAT_LIST']).split(' | ')
-        qtys = str(row['KUANTITI']).split(' | ')
-        for u, q in zip(ubats, qtys):
-            data_list.append({
-                'NAMA PESAKIT': str(row['NAMA']).strip().upper(),
-                'UBAT': str(u).strip().upper(), # Tukar ke UPPER untuk sorting yang betul
-                'KUANTITI_TEXT': str(q).strip(),
-                'KUANTITI_VAL': ekstrak_angka(q)
-            })
-    
-    if not data_list: return pd.DataFrame()
-    
-    temp_df = pd.DataFrame(data_list)
-    
-    # 1. Pivot Table untuk paparan melintang
-    matrix = temp_df.pivot_table(index='UBAT', columns='NAMA PESAKIT', values='KUANTITI_TEXT', aggfunc='first').fillna('')
-    
-    # 2. Susun Index A-Z (Upper case menjamin susunan yang betul)
-    matrix = matrix.sort_index()
-    
-    # 3. Kira TOTAL
-    total_val = temp_df.groupby('UBAT')['KUANTITI_VAL'].sum()
-    matrix.insert(0, 'TOTAL (BIJI/UNIT)', total_val)
-    
-    return matrix
+        n_pesakit = str(row['NAMA']).strip().upper()
+        u_list = str(row['UBAT_LIST']).split(' | ')
+        q_list = str(row['KUANTITI']).split(' | ')
+        
+        for u, q in zip(u_list, q_list):
+            u_clean = u.strip().upper()
+            if u_clean in MASTER_UBAT:
+                # Simpan kuantiti teks untuk paparan pesakit
+                matrix.at[u_clean, n_pesakit] = q.strip()
+                # Simpan nilai angka untuk kira TOTAL
+                try:
+                    num = int(''.join(filter(str.isdigit, q)))
+                except: num = 0
+                pesakit_data.append({'U': u_clean, 'V': num})
+
+    # 3. Kira TOTAL (Hanya ambil dari data pesakit tadi)
+    if pesakit_data:
+        df_calc = pd.DataFrame(pesakit_data)
+        totals = df_calc.groupby('U')['V'].sum()
+        matrix.insert(0, "📊 TOTAL (BIJI)", totals)
+    else:
+        matrix.insert(0, "📊 TOTAL (BIJI)", 0)
+
+    return matrix.fillna("")
 
 if 'bakul' not in st.session_state: st.session_state.bakul = []
 
 # --- UI ---
-menu = st.sidebar.radio("NAVIGASI", ["📝 INPUT", "📊 SUMMARY"])
+menu = st.sidebar.radio("MENU", ["📝 INPUT", "📊 SUMMARY"])
 
 if menu == "📝 INPUT":
-    st.header("Input Pesakit & Ubat")
-    with st.form("main_form"):
-        c1, c2, c3 = st.columns(3)
+    st.header("Pendaftaran Pesakit")
+    with st.container(border=True):
+        c1, c2 = st.columns(2)
         nama = c1.text_input("Nama:").upper()
         ic = c2.text_input("IC:")
-        batch = c3.selectbox("Batch:", [f"{m} - Batch {b}" for m in ["Mac", "April", "Mei", "Jun", "Julai", "Ogos", "September", "Oktober", "November", "Disember"] for b in [1, 2]])
+        batch = st.selectbox("Batch:", [f"{m} - Batch {b}" for m in ["Mac", "April", "Mei", "Jun", "Julai", "Ogos", "September", "Oktober", "November", "Disember"] for b in [1, 2]])
         
-        st.write("---")
         u1, u2 = st.columns([3, 1])
-        pilih_u = u1.selectbox("Pilih Ubat:", ["-- PILIH --"] + MASTER_UBAT)
-        qty_u = u2.text_input("Qty (Contoh: 30):")
+        p_u = u1.selectbox("Ubat:", ["-- PILIH --"] + MASTER_UBAT)
+        p_q = u2.text_input("Qty:")
         
-        if st.form_submit_button("➕ Tambah ke Bakul"):
-            if pilih_u != "-- PILIH --" and qty_u:
-                st.session_state.bakul.append({"u": pilih_u, "q": qty_u})
+        if st.button("➕ Tambah Ubat"):
+            if p_u != "-- PILIH --" and p_q:
+                st.session_state.bakul.append({"u": p_u, "q": p_q})
+                st.rerun()
 
     if st.session_state.bakul:
         st.table(pd.DataFrame(st.session_state.bakul))
-        if st.button("💾 SIMPAN SEMUA"):
-            u_list = " | ".join([x['u'] for x in st.session_state.bakul])
-            q_list = " | ".join([x['q'] for x in st.session_state.bakul])
-            payload = {"Nama": nama, "IC": ic, "Ubat_List": u_list, "Kuantiti": q_list, "Batch": batch}
-            r = requests.post(URL_API, json=payload)
-            if r.status_code == 200:
-                st.success("Tersimpan!"); st.session_state.bakul = []; st.balloons()
+        if st.button("💾 SIMPAN SEMUA", type="primary"):
+            u_str = " | ".join([x['u'] for x in st.session_state.bakul])
+            q_str = " | ".join([x['q'] for x in st.session_state.bakul])
+            requests.post(URL_API, json={"Nama": nama, "IC": ic, "Ubat_List": u_str, "Kuantiti": q_str, "Batch": batch})
+            st.success("Data Berjaya Disimpan!"); st.session_state.bakul = []; st.balloons()
 
 elif menu == "📊 SUMMARY":
-    st.header("Checklist & Total")
+    st.header("Checklist Master (A-Z)")
     df = load_data()
     if not df.empty:
-        batch_sel = st.selectbox("Tapis Batch:", sorted(df['BATCH'].unique()))
-        df_f = df[df['BATCH'] == batch_sel]
-        res = convert_to_matrix_with_total(df_f)
-        st.dataframe(res, use_container_width=True)
-        st.download_button("📥 Download CSV", res.to_csv().encode('utf-8'), "Checklist.csv")
+        b_sel = st.selectbox("Pilih Batch:", sorted(df['BATCH'].unique()))
+        df_f = df[df['BATCH'] == b_sel]
+        
+        # Jana matrix Checklist Penuh
+        final_table = convert_to_matrix_full(df_f)
+        
+        st.dataframe(final_table, use_container_width=True, height=600)
+        st.download_button("📥 Muat Turun CSV", final_table.to_csv().encode('utf-8'), f"Checklist_{b_sel}.csv")
